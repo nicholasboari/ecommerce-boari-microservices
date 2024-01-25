c5c025eb42ee936cfae3864614e441cb8dbf1412
nicholasboari <nicholasboari@gmail.com>
2024-01-25 20:49:55 -0300

    fix: gateway routes, feat: security product service

--- product-service/src/main/java/com/ecommerceboari/productservice/security/JwtAuthConverter.java
+++ product-service/src/main/java/com/ecommerceboari/productservice/security/JwtAuthConverter.java
@@ -1,6 +1,5 @@
-package com.ecommerceboari.gateway.security;
+package com.ecommerceboari.productservice.security;
 
-import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.lang.NonNull;
@@ -12,31 +11,36 @@ import org.springframework.security.oauth2.jwt.JwtClaimNames;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.stereotype.Component;
-import reactor.core.publisher.Mono;
 
-import java.util.*;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @Component
-public class JwtAuthConverter implements Converter<Jwt, Mono<? extends AbstractAuthenticationToken>> {
+public class JwtAuthConverter implements Converter<Jwt, AbstractAuthenticationToken> {
 
-    private final JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+    private final JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter =
+            new JwtGrantedAuthoritiesConverter();
 
     @Value("${jwt.auth.converter.principle-attribute}")
     private String principleAttribute;
-
     @Value("${jwt.auth.converter.resource-id}")
     private String resourceId;
 
     @Override
-    public Mono<? extends AbstractAuthenticationToken> convert(@NonNull Jwt jwt) {
+    public AbstractAuthenticationToken convert(@NonNull Jwt jwt) {
         Collection<GrantedAuthority> authorities = Stream.concat(
-                        jwtGrantedAuthoritiesConverter.convert(jwt).stream(), extractResourceRoles(jwt)
-                                .stream())
-                .collect(Collectors.toSet());
+                jwtGrantedAuthoritiesConverter.convert(jwt).stream(),
+                extractResourceRoles(jwt).stream()
+        ).collect(Collectors.toSet());
 
-        return Mono.just(new JwtAuthenticationToken(jwt, authorities, getPrincipleClaimName(jwt)));
+        return new JwtAuthenticationToken(
+                jwt,
+                authorities,
+                getPrincipleClaimName(jwt)
+        );
     }
 
     private String getPrincipleClaimName(Jwt jwt) {
@@ -47,24 +51,24 @@ public class JwtAuthConverter implements Converter<Jwt, Mono<? extends AbstractA
         return jwt.getClaim(claimName);
     }
 
-
     private Collection<? extends GrantedAuthority> extractResourceRoles(Jwt jwt) {
         Map<String, Object> resourceAccess;
         Map<String, Object> resource;
         Collection<String> resourceRoles;
-
         if (jwt.getClaim("resource_access") == null) {
             return Set.of();
         }
-
         resourceAccess = jwt.getClaim("resource_access");
+
         if (resourceAccess.get(resourceId) == null) {
             return Set.of();
         }
         resource = (Map<String, Object>) resourceAccess.get(resourceId);
+
         resourceRoles = (Collection<String>) resource.get("roles");
         return resourceRoles
                 .stream()
-                .map(role -> new SimpleGrantedAuthority("ROLE_" + role)).collect(Collectors.toSet());
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                .collect(Collectors.toSet());
     }
 }
---
+++ product-service/src/main/java/com/ecommerceboari/productservice/security/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.ecommerceboari.productservice.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtAuthConverter jwtAuthConverter;
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.ignoringRequestMatchers(toH2Console()).disable())
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(toH2Console()).permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/v1/products/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/v1/products/**").hasRole("client_admin")
+                        .requestMatchers(HttpMethod.PUT, "/api/v1/products/**").hasRole("client_admin")
+                        .requestMatchers(HttpMethod.DELETE, "/api/v1/products/**").hasRole("client_admin")
+                        .anyRequest().authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt
+                                .jwtAuthenticationConverter(jwtAuthConverter)
+                        ));
+        return http.build();
+    }
+}
